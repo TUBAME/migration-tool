8d4593245938bb4caf99e0250e34352ee29f698e
TUBAME <tubame@lab.ntt.co.jp>
2013-09-26 22:22:30 +0900

    削除

--- src/org.tubame.portability/src/org/tubame/portability/model/generated/weblogic/wlcmp/wlv5/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides weblogicVer5 convert XML field model (JAXB generated automatically places).<br/>
- * 
- */
-package org.tubame.portability.model.generated.weblogic.wlcmp.wlv5;
-
--- src/org.tubame.portability/src/org/tubame/portability/model/generated/weblogic/wlejbjar/wlv8/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Provides a weblogic-wlejbjarVer8 convert XML field model (JAXB generated automatically places).<br/>
- * 
- */
-package org.tubame.portability.model.generated.weblogic.wlejbjar.wlv8;
-
