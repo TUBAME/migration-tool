dccb15fff9d99198eb38169d9a09487033f1c54a
kaoru.watanabe <watanabe_kaoru_b1@lab.ntt.co.jp>
2014-11-06 17:24:25 +0900

    依存性検索の検索対象のプロジェクトがworkspaceにデータをコピーしていない場合に検索できるように修正した。

--- src/tubame.wsearch/src/tubame/wsearch/util/PluginUtil.java
+++ src/tubame.wsearch/src/tubame/wsearch/util/PluginUtil.java
@@ -18,6 +18,7 @@
  */
 package tubame.wsearch.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 
@@ -34,9 +35,11 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+
 import tubame.wsearch.Activator;
 
 /**
@@ -45,164 +48,197 @@ import tubame.wsearch.Activator;
  */
 public class PluginUtil {
 
-    /**
-     * Default constructor.<br/>
-     * Prohibits the instantiation from other classes.<br/>
-     */
-    private PluginUtil() {
-        // no operation
-    }
+	/**
+	 * Default constructor.<br/>
+	 * Prohibits the instantiation from other classes.<br/>
+	 */
+	private PluginUtil() {
+		// no operation
+	}
 
-    /**
-     * Get the workbench window active.<br/>
-     * 
-     * @return Workbench window active
-     */
-    public static IWorkbenchWindow getActiveWorkbenchWindow() {
-        return Activator.getDefault().getWorkbench().getActiveWorkbenchWindow();
-    }
+	/**
+	 * Get the workbench window active.<br/>
+	 * 
+	 * @return Workbench window active
+	 */
+	public static IWorkbenchWindow getActiveWorkbenchWindow() {
+		return Activator.getDefault().getWorkbench().getActiveWorkbenchWindow();
+	}
 
-    /**
-     * Get the workbench window active.<br/>
-     * 
-     * @return Workbench window active
-     */
-    public static IWorkbenchPage getActivePage() {
-        return Activator.getDefault().getWorkbench().getActiveWorkbenchWindow()
-                .getActivePage();
-    }
+	/**
+	 * Get the workbench window active.<br/>
+	 * 
+	 * @return Workbench window active
+	 */
+	public static IWorkbenchPage getActivePage() {
+		return Activator.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage();
+	}
 
-    /**
-     * Get the workbench shell active.<br/>
-     * 
-     * @return Workbench shell active
-     */
-    public static Shell getActiveWorkbenchShell() {
-        IWorkbenchWindow window = getActiveWorkbenchWindow();
-        return window != null ? window.getShell() : getStandardDisplay()
-                .getActiveShell();
-    }
+	/**
+	 * Get the workbench shell active.<br/>
+	 * 
+	 * @return Workbench shell active
+	 */
+	public static Shell getActiveWorkbenchShell() {
+		IWorkbenchWindow window = getActiveWorkbenchWindow();
+		return window != null ? window.getShell() : getStandardDisplay().getActiveShell();
+	}
 
-    /**
-     * Get the display.<br/>
-     * Thread is calling this method if in possession of the relevant display<br/>
-     * Get the associated display, get the default display if not in possession.<br/>
-     * 
-     * @return Display
-     */
-    public static Display getStandardDisplay() {
-        Display display = Display.getCurrent();
-        if (display == null) {
-            display = Display.getDefault();
-        }
-        return display;
-    }
+	/**
+	 * Get the display.<br/>
+	 * Thread is calling this method if in possession of the relevant display<br/>
+	 * Get the associated display, get the default display if not in possession.<br/>
+	 * 
+	 * @return Display
+	 */
+	public static Display getStandardDisplay() {
+		Display display = Display.getCurrent();
+		if (display == null) {
+			display = Display.getDefault();
+		}
+		return display;
+	}
 
-    /**
-     * Get the projects selected.<br/>
-     * Get the projects selected. If the project selected does not exist, then
-     * return null.<br/>
-     * 
-     * @return Project selection
-     */
-    public static IProject getSelectedProject() {
-        IWorkbenchWindow window = PlatformUI.getWorkbench()
-                .getActiveWorkbenchWindow();
-        ISelection selection = window.getActivePage().getSelection();
-        if (selection instanceof IStructuredSelection) {
-            Object element = ((IStructuredSelection) selection)
-                    .getFirstElement();
-            if (element instanceof IResource) {
-                return ((IResource) element).getProject();
-            } else if (element instanceof IJavaElement) {
-                String projectName = ((IJavaElement) element).getJavaProject()
-                        .getElementName();
-                return ResourcesPlugin.getWorkspace().getRoot()
-                        .getProject(projectName);
-            }
-        }
-        return null;
-    }
+	/**
+	 * Get the projects selected.<br/>
+	 * Get the projects selected. If the project selected does not exist, then
+	 * return null.<br/>
+	 * 
+	 * @return Project selection
+	 */
+	public static IProject getSelectedProject() {
+		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		ISelection selection = window.getActivePage().getSelection();
+		if (selection instanceof IStructuredSelection) {
+			Object element = ((IStructuredSelection) selection).getFirstElement();
+			if (element instanceof IResource) {
+				return ((IResource) element).getProject();
+			} else if (element instanceof IJavaElement) {
+				String projectName = ((IJavaElement) element).getJavaProject().getElementName();
+				return ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Display an error message.<br/>
-     * 
-     * @param title
-     *            Title of the dialog
-     * @param message
-     *            Message of the dialog
-     * @param e
-     *            Exception
-     */
-    public static void viewErrorDialog(String title, String message, Throwable e) {
-        MessageDialog.openError(getActiveWorkbenchShell(), title, message);
-    }
+	public static IProject findIProjectByPath(String targetFullPath) {
+		IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		for (IProject project : projects) {
+			if (targetFullPath.startsWith(project.getLocation().toOSString())) {
+				return project;
+			}
+		}
+		return null;
 
-    /**
-     * Get the project name from the file path.<br/>
-     * Get the project name from the full path from the workspace root.<br/>
-     * 
-     * @param filePath
-     *            File path from the workspace root
-     * @return Project name
-     */
-    public static String getProjectName(String filePath) {
-        String temp = filePath;
-        temp = temp.replaceAll("\\\\", CmnStringUtil.SLASH);
-        if (!temp.contains(CmnStringUtil.SLASH)) {
-            return temp;
-        }
-        return temp.substring(0, temp.indexOf(CmnStringUtil.SLASH));
-    }
+	}
 
-    /**
-     * Get the IProject from the project name.<br/>
-     * Create a IProject from the project name, get.<br/>
-     * 
-     * @param projectName
-     *            Project name
-     * @return IProject
-     */
-    public static IProject getProject(String projectName) {
-        if (CmnStringUtil.isEmpty(projectName)) {
-            return null;
-        }
-        return ResourcesPlugin.getWorkspace().getRoot()
-                .getProject(PluginUtil.getProjectName(projectName));
-    }
+	/**
+	 * Display an error message.<br/>
+	 * 
+	 * @param title
+	 *            Title of the dialog
+	 * @param message
+	 *            Message of the dialog
+	 * @param e
+	 *            Exception
+	 */
+	public static void viewErrorDialog(String title, String message, Throwable e) {
+		MessageDialog.openError(getActiveWorkbenchShell(), title, message);
+	}
 
-    /**
-     * Get projects included in the file path is currently open.<br/>
-     * get projects included in the file path from the root workspace is
-     * currently open or.<br/>
-     * 
-     * @param filePath
-     *            File path from the workspace root
-     * @return True if open, return false if not open
-     */
-    public static boolean isProjectOpen(String filePath) {
-        if (ResourcesPlugin.getWorkspace().getRoot()
-                .getProject(PluginUtil.getProjectName(filePath)).isOpen()) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * Get the project name from the file path.<br/>
+	 * Get the project name from the full path from the workspace root.<br/>
+	 * 
+	 * @param filePath
+	 *            File path from the workspace root
+	 * @return Project name
+	 */
+	public static String getProjectName(String filePath) {
+		String temp = filePath;
+		temp = temp.replaceAll("\\\\", CmnStringUtil.SLASH);
+		if (!temp.contains(CmnStringUtil.SLASH)) {
+			return temp;
+		}
+		return temp.substring(0, temp.indexOf(CmnStringUtil.SLASH));
+	}
 
-    /**
-     * Get the Plugin directory.<br/>
-     * 
-     * @return Own Plugin directory
-     */
-    public static String getPluginDir() {
-        String pluginDirectory = CmnStringUtil.EMPTY;
-        try {
-            URL entry = Activator.getDefault().getBundle()
-                    .getEntry(CmnFileUtil.FILE_SEPARATOR);
-            pluginDirectory = FileLocator.resolve(entry).getPath();
-        } catch (IOException e) {
+	/**
+	 * Get the IProject from the project name.<br/>
+	 * Create a IProject from the project name, get.<br/>
+	 * 
+	 * @param projectName
+	 *            Project name
+	 * @return IProject
+	 */
+	public static IProject getProject(String projectName) {
+		if (CmnStringUtil.isEmpty(projectName)) {
+			return null;
+		}
+		return ResourcesPlugin.getWorkspace().getRoot().getProject(PluginUtil.getProjectName(projectName));
+	}
 
-        }
-        return pluginDirectory;
-    }
+	/**
+	 * Get projects included in the file path is currently open.<br/>
+	 * get projects included in the file path from the root workspace is
+	 * currently open or.<br/>
+	 * 
+	 * @param filePath
+	 *            File path from the workspace root
+	 * @return True if open, return false if not open
+	 */
+	public static boolean isProjectOpen(String filePath) {
+		if (ResourcesPlugin.getWorkspace().getRoot().getProject(PluginUtil.getProjectName(filePath)).isOpen()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * Get the Plugin directory.<br/>
+	 * 
+	 * @return Own Plugin directory
+	 */
+	public static String getPluginDir() {
+		String pluginDirectory = CmnStringUtil.EMPTY;
+		try {
+			URL entry = Activator.getDefault().getBundle().getEntry(CmnFileUtil.FILE_SEPARATOR);
+			pluginDirectory = FileLocator.resolve(entry).getPath();
+		} catch (IOException e) {
+
+		}
+		return pluginDirectory;
+	}
+
+	public static String getRealOutputDirPath(IProject selectedProject, String outputDirPath) {
+		String outputPathExcludeProjectName = getPathExcludeProjectName(selectedProject.getName(), outputDirPath);
+		if (outputPathExcludeProjectName == null) {
+			return selectedProject.getLocation().toOSString();
+		}
+		return selectedProject.getFile(outputPathExcludeProjectName).getLocation().toOSString();
+	}
+
+	private static String getPathExcludeProjectName(String projectName, String target) {
+		if (projectName.equals(target)) {
+			return null;
+		} else {
+			return target.substring(projectName.length());
+		}
+	}
+
+	public static String getRelativePathBaseProject(String targetFullPath) {
+		return targetFullPath.substring(PluginUtil.getSelectedProject().getLocation().toOSString().length());
+	}
+
+	public static String getCanonicalPath(String target) {
+		String str = null;
+		File file = new File(target);
+		try {
+			str = file.getCanonicalPath();
+			return str;
+		} catch (IOException e) {
+			throw new IllegalStateException(e);
+		}
+	}
 }
