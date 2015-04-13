80b11d45b10b28dd3ec7e05cf2975a6a8cba9658
watanabe kaoru <azkaoru@gmail.com>
2015-04-13 18:12:33 +0900

    不要なものを削除

--- src/tubame.wsearch/src/tubame/wsearch/logics/WSearchCsvReader.java
+++ src/tubame.wsearch/src/tubame/wsearch/logics/WSearchCsvReader.java
@@ -311,7 +311,6 @@ public class WSearchCsvReader implements WSearchReader {
             return rowList;
 
         } catch (Exception e) {
-        	e.printStackTrace();
             throw new WSearchPluginException(e);
         }
     }
