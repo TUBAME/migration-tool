7d6d7cf8dbbe7e6da96100b88d04e02124470277
watanabe kaoru <azkaoru@gmail.com>
2017-09-26 17:46:47 +0900

    remove ext_search_sql_parser.py

--- src/tubame.portability/resources/tubame-search-modules/test/search_target/testTubameSqlSearchOrCondition/test.sql
+++
@@ -1,4 +0,0 @@
-select dt, EXTRACT(YEAR FROM dt),
- EXTRACT(MONTH FROM dt), EXTRACT(DAY FROM dt) from date_sample;
-
-select emp_id, dept_id, salary,var_samp( salary ) over( partition by dept_id ) as var_samp from emp;
